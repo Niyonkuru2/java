d21c6a7b888ca810f5184b673a2b0122b0c54ace
niyokuru2 <niyonkurusam5@gmail.com>
2025-11-13 15:00:45 +0200

    delete

--- foodwagen/types/food.ts
+++
@@ -1,8 +0,0 @@
-export interface Food {
-  food_name: string;
-  food_rating: string;
-  food_image: string;
-  restaurant_name: string;
-  restaurant_logo: string;
-  restaurant_status: "Open Now" | "Closed";
-}
